17ea920cc51f19274a9029fcb9cc9bf224a7eb3d
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-01-21 14:37:42 +0800

    Add greeting messages

--- src/main/java/Fauna.java
+++ src/main/java/Fauna.java
@@ -1,4 +1,6 @@
-public class Duke {
+public class Fauna {
+    private static final String chatbotName = "Fauna";
+
 
     public static void main(String[] args) {
         String logo = """
@@ -7,9 +9,16 @@ public class Duke {
             | |_ / _ \\| | | |  \\| | / _ \\  
             |  _/ ___ \\ |_| | |\\  |/ ___ \\ 
             |_|/_/   \\_\\___/|_| \\_/_/   \\_\\""";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n");
+
+        // greet the user
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // exit
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
-
-
-
