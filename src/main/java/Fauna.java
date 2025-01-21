fcb6b5db01d1ce8af0f3080eb8e24061b718d820
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-01-21 14:33:24 +0800

    Rename main class and change logo

---
+++ src/main/java/Fauna.java
@@ -0,0 +1,15 @@
+public class Duke {
+
+    public static void main(String[] args) {
+        String logo = """
+             _____ _   _   _ _   _    _    
+            |  ___/ \\ | | | | \\ | |  / \\   
+            | |_ / _ \\| | | |  \\| | / _ \\  
+            |  _/ ___ \\ |_| | |\\  |/ ___ \\ 
+            |_|/_/   \\_\\___/|_| \\_/_/   \\_\\""";
+        System.out.println("Hello from\n" + logo);
+    }
+}
+
+
+
