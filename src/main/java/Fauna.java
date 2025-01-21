8a3353ed59c61fb81f9d7d394d7ab9adff264976
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-01-21 14:52:51 +0800

    Add echo feature

--- src/main/java/Fauna.java
+++ src/main/java/Fauna.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Fauna {
     private static final String chatbotName = "Fauna";
 
+    // level-1
+    private static void chatbotEcho() {
+        // loop user chatbot conversation
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            // get user's input
+            String userInput = sc.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            // echo user's input
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________\n");
+        }
+    }
 
     public static void main(String[] args) {
         String logo = """
@@ -15,7 +35,10 @@ public class Fauna {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
+
+        // Level-1 Echo
+        chatbotEcho();
 
         // exit
         System.out.println("Bye. Hope to see you again soon!");
